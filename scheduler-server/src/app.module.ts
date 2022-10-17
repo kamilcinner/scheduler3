98f669fdb2dcaf3368348158929f8c2d38dd309e
Kamil Cinner <kamil.cinner@gmail.com>
2022-10-17 22:36:03 +0200

    [SERVER] Added typeorm and sqlite

--- scheduler-server/src/app.module.ts
+++ scheduler-server/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Task } from './tasks/entities/task.entity';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      synchronize: true,
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [Task],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
