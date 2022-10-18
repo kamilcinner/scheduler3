d04174102c7aa085d28212f2c86e512bb7e26cb1
Kamil Cinner <kamil.cinner@gmail.com>
2022-10-19 00:34:37 +0200

    [WEBAPP] Added Angular ESLint plugin

--- scheduler-webapp/.eslintrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  root: true,
-  ignorePatterns: ['karma.conf.js'],
-};
